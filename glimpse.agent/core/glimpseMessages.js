2126f639c71638817b675767cbaf62fac921a4f8
Mike Kaufman <mkaufman@microsoft.com>
2015-09-29 13:56:45 -0700

    adding user-id message to send a fake user identity.  This get's the current prototype UI to work.

--- glimpse.agent/core/glimpseMessages.js
+++ glimpse.agent/core/glimpseMessages.js
@@ -1,134 +1,149 @@
 var http = require('http');
-var uuid = require ('node-uuid'); 
+var uuid = require('node-uuid');
 var util = require('util');
 
 function Context() {
-	this.id = uuid.v4();
-	this.Type = "Request"
+    this.id = uuid.v4();
+    this.Type = "Request"
 }
 
-var getNextOrdinal = (function() {
-  var i = 0;  
-  
-  return function() {
-    return ++i;
-  };
+var getNextOrdinal = (function () {
+    var i = 0;
+
+    return function () {
+        return ++i;
+    };
 })();
 
 function createMessage(data, indices, types, context) {
     var message = {};
-      message.id = uuid.v4();
-	message.context = context;
-	message.ordinal = getNextOrdinal();
+    message.id = uuid.v4();
+    message.context = context;
+    message.ordinal = getNextOrdinal();
 
     message.types = types;
-	  message.indices =  indices;
-	  var payloadObject = {
-  		id: message.id,
-	  	payload: data,
-		  ordinal: message.ordinal,
-		  context: message.context,
-      types: types
-	  };  
-	  message.payload = JSON.stringify(payloadObject);
-    
-      
-   return message;
-  
+    message.indices = indices;
+    var payloadObject = {
+        id: message.id,
+        payload: data,
+        ordinal: message.ordinal,
+        context: message.context,
+        types: types
+    };
+    message.payload = JSON.stringify(payloadObject);
+
+
+    return message;
+
 }
 
 function beginRequestMessage(context, hostUrl) {
 
-    var data = {url: hostUrl};
-	  var indices =  {"request-url":hostUrl}
+    var data = { url: hostUrl };
+    var indices = { "request-url": hostUrl }
     var types = ["begin-request-message"];
-      
-    var message = createMessage(data,  indices, types, context);
-  return message;
-}  
-  
-function endRequestMessage(context, hostUrl, beginTime, endTime) {
 
-  var duration = endTime.getTime() - beginTime.getTime();
-
-	var data = {
-		 duration : duration,
-         startTime : beginTime,
-         endTime:endTime,
-         url:hostUrl,
-         method:"GET",
-         contentType :"text/html; charset=utf-8",
-         statusCode:200
-	};
-
-	var indices = {
-		 "request-duration": duration,
-         "request-datetime":beginTime,
-         "request-url":hostUrl,
-         "request-method":"GET",
-         "request-content-type":"text/html; charset=utf-8",
-         "request-status-code":200 };
-
-  var types = ["end-request-message"];
-
-
-  var message = createMessage(data, indices, types, context);
-  return message;
+    var message = createMessage(data, indices, types, context);
+    return message;
 }
 
+function endRequestMessage(context, hostUrl, beginTime, endTime) {
+
+    var duration = endTime.getTime() - beginTime.getTime();
+
+    var data = {
+        duration: duration,
+        startTime: beginTime,
+        endTime: endTime,
+        url: hostUrl,
+        method: "GET",
+        contentType: "text/html; charset=utf-8",
+        statusCode: 200
+    };
+
+    var indices = {
+        "request-duration": duration,
+        "request-datetime": beginTime,
+        "request-url": hostUrl,
+        "request-method": "GET",
+        "request-content-type": "text/html; charset=utf-8",
+        "request-status-code": 200
+    };
+
+    var types = ["end-request-message"];
+
+
+    var message = createMessage(data, indices, types, context);
+    return message;
+}
+
+function userIDMessage(context, userId, userName, email, image) {
+
+    var data = {
+        userId: userId,
+        username: userName,
+        email: email,
+        image: image
+    };
+
+    var types = ["user-identification"];
+
+    var message = createMessage(data, null, types, context);
+    return message;
+}
+
+
 function GlimpseAgent() {
-	var that = this;
-	
-	that.send = function (messages) {
-		
-		// convert a single message into an array of messages
-		if( !util.isArray(messages)) {
-            messages = [messages]; 
+    var that = this;
+
+    that.send = function (messages) {
+
+        // convert a single message into an array of messages
+        if (!util.isArray(messages)) {
+            messages = [messages];
         }
-		
-		var str  = '';
-        var callback = function(response) {
-	        console.log("GlimpseAgent:  callback started");
-	        response.on('data', function(chunk) {
-		        str += chunk;
-	        });
-	
-	        response.on('end', function() {
-		        console.log("GlimpseAgent:  end:  " + str);
-	        });
-		}
-	 
-	    var options = {
-	        host:  'localhost',
-	        path: '/glimpse/AgentMessage',
- 	        port:5000,
-	        method: 'POST'
+
+        var str = '';
+        var callback = function (response) {
+            console.log("GlimpseAgent:  callback started");
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
+
+            response.on('end', function () {
+                console.log("GlimpseAgent:  end:  " + str);
+            });
+        }
+
+        var options = {
+            host: 'localhost',
+            path: '/glimpse/AgentMessage',
+            port: 5000,
+            method: 'POST'
         };
-	
-	    var req = http.request(options, callback);
+
+        var req = http.request(options, callback);
 
         console.log("calling write");
-		var json = JSON.stringify(messages);
-    
-    console.log(json);
-    
+        var json = JSON.stringify(messages);
+
+        console.log(json);
+
         req.write(json);
         console.log("calling end");
         req.end();
 
         console.log("DONE!")
-    }	
+    }
 }
-	
-
-
 
 
 var glimpseMessages = {
-	GlimpseAgent: GlimpseAgent,
-	Context: Context,
-	beginRequestMessage: beginRequestMessage,
-  endRequestMessage: endRequestMessage
+    GlimpseAgent: GlimpseAgent,
+    Context: Context,
+    beginRequestMessage: beginRequestMessage,
+    endRequestMessage: endRequestMessage,
+    userIDMessage: userIDMessage
 };
 
-module.exports =  glimpseMessages;
+module.exports = glimpseMessages;
--- glimpse.agent/core/glimpseMiddleware.js
+++ glimpse.agent/core/glimpseMiddleware.js
@@ -1,6 +1,7 @@
 var useragent = require('useragent');
 var glimpseMessages = require('./glimpseMessages.js');
 var runtime = require('./glimpseRuntime.js');
+var uuid = require('node-uuid');
 
 
 // TODO: Need to do this better 
@@ -9,7 +10,7 @@ var runtime = require('./glimpseRuntime.js');
 // config.tabs.push(reqResTab);
 
 
-function glimpseMiddleware (req, res, next) {
+function glimpseMiddleware(req, res, next) {
     console.log('EXPRESS - Begin Request - ' + req.path);
 
     var glimpseContext = new glimpseMessages.Context();
@@ -17,7 +18,7 @@ function glimpseMiddleware (req, res, next) {
 
     // TODO: gotta be a better way to do this
     //console.log("res.end is" + res.end);
-    
+
     res.end = bindResponseEnd(req, res, glimpseContext, glimpseAgent, new Date());
 
     runtime.beginRequest({
@@ -28,22 +29,24 @@ function glimpseMiddleware (req, res, next) {
             // TODO, hopefully this doesn't break
             // but we should still call next if it does
         }
-         
-        var message = glimpseMessages.beginRequestMessage(glimpseContext, req.originalUrl);
-        glimpseAgent.send(message);
-        
+
+        // TODO - correctly create the user ID message
+        var userIdMesage = glimpseMessages.userIDMessage(glimpseContext, uuid.v4(), "5414", null, "https://www.gravatar.com/avatar/D5F856634208232580DC59FC18E0414E.jpg?d=identicon");
+        var beginRequestMessage = glimpseMessages.beginRequestMessage(glimpseContext, req.originalUrl);
+        glimpseAgent.send([beginRequestMessage, userIdMesage]);
+
         next();
     });
 }
 
-function bindResponseEnd (req, res, glimpseContext, glimpseAgent, beginTime) {
+function bindResponseEnd(req, res, glimpseContext, glimpseAgent, beginTime) {
     var resEnd = res.end;
-    function glimpseResEnd (data, encoding, callback) {
+    function glimpseResEnd(data, encoding, callback) {
         console.log('EXPRESS - End Request');
 
         var that = this;
         //var endArgs = Array.prototype.slice.call(arguments, 2);
- 
+
         runtime.endRequest({
             request: req,
             response: res
@@ -55,40 +58,40 @@ function bindResponseEnd (req, res, glimpseContext, glimpseAgent, beginTime) {
 
             console.log("we are here in endRequest");
 
-            var message =  glimpseMessages.endRequestMessage(glimpseContext, req.originalUrl, beginTime, new Date());
+            var message = glimpseMessages.endRequestMessage(glimpseContext, req.originalUrl, beginTime, new Date());
             glimpseAgent.send(message);
-            
-                        console.log("calling resEnd.Apply");
+
+            console.log("calling resEnd.Apply");
             resEnd.call(that, data, encoding, callback);
 
-// 
-// 
-//             // TODO, switch endRequest and injectScript over to use
-//             // async.series() or similar
-//             runtime.injectScript({
-//                 request: req,
-//                 response: res,
-//                 data: data,
-//                 encoding: encoding
-//             }, function (err, result) {
-//                 if (err) {
-//                     // TODO, hopefully this doesn't break
-//                     // but we should still call next if it does
-//                 }
-// 
-//                 result = result || {};
-// 
-//                 endArgs.unshift(result.encoding || encoding);
-//                 endArgs.unshift(result.data || data);
-// 
-//                 resEnd.apply(that, endArgs);
-//            }); 
+            // 
+            // 
+            //             // TODO, switch endRequest and injectScript over to use
+            //             // async.series() or similar
+            //             runtime.injectScript({
+            //                 request: req,
+            //                 response: res,
+            //                 data: data,
+            //                 encoding: encoding
+            //             }, function (err, result) {
+            //                 if (err) {
+            //                     // TODO, hopefully this doesn't break
+            //                     // but we should still call next if it does
+            //                 }
+            // 
+            //                 result = result || {};
+            // 
+            //                 endArgs.unshift(result.encoding || encoding);
+            //                 endArgs.unshift(result.data || data);
+            // 
+            //                 resEnd.apply(that, endArgs);
+            //            }); 
 
- 
-        }); 
+
+        });
     }
 
     return glimpseResEnd;
 }
 
-module.exports = { glimpseMiddleware } ;
+module.exports = { glimpseMiddleware };
